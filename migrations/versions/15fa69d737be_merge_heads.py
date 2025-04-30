e3f3a0c1843e9a8783c448d17d05c375c57bdec5
Veronica Lyu <24516605@student.uwa.edu.au>
2025-04-30 12:57:43 +0800

    chore: merged alembic heads to resolve multiple head issue

---
+++ migrations/versions/15fa69d737be_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 15fa69d737be
+Revises: 
+Create Date: 2025-04-30 12:53:31.233552
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '15fa69d737be'
+down_revision = ('1f487c034e91', 'ea5589dda710')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
